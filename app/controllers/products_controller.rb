283031f047d75f0549a9eccf270bee3dd64e71b8
Sebastian Sim <phistando@yahoo.com>
2016-08-04 15:24:35 +0800

    revised actions right for logged in users

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-  # before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  # before_action :correct_user,   only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :create, :new]
+  # before_action :correct_user,   only: [:index, :edit, :update, :destroy]
   # before_action :admin_user,     only: :destroy
 
 
